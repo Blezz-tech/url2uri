28c4db4886925b56ef385c8e5a1fae0b98384f54
blezz-tech <markus.jenya04@yandex.ru>
2025-07-01 20:17:03 +0300

    fix

--- urk2uri.sh
+++ urk2uri.sh
@@ -1,18 +1,37 @@
-# Функция декодирования URL, кроме %20 (пробелов)
 decode_except_spaces() {
     local input="$1"
-    # Сначала заменим %20 на уникальный маркер, чтобы не трогать пробелы
     local marker="___SPACE___"
+
+    # Заменяем %20 на маркер
     local tmp="${input//%20/$marker}"
 
-    # Декодируем все остальные %XX
-    # Заменяем %XX на \xXX для printf
-    tmp=$(echo -e "$(sed -E 's/%([0-9A-Fa-f]{2})/\\x\1/g' <<< "$tmp")")
+    # Функция для замены %XX на соответствующие символы
+    # Используем цикл и printf для декодирования
+    local output=""
+    local i=0
+    local len=${#tmp}
 
-    # Восстанавливаем пробелы
-    tmp="${tmp//$marker/%20}"
+    while (( i < len )); do
+        local c="${tmp:i:1}"
+        if [[ "$c" == "%" && i+2 < len ]]; then
+            local hex="${tmp:i+1:2}"
+            # Проверяем, что hex - это валидный шестнадцатеричный код
+            if [[ "$hex" =~ ^[0-9A-Fa-f]{2}$ ]]; then
+                # Конвертируем hex в символ
+                printf -v decoded_char "\\x$hex"
+                output+="$decoded_char"
+                ((i+=3))
+                continue
+            fi
+        fi
+        output+="$c"
+        ((i++))
+    done
 
-    echo "$tmp"
+    # Восстанавливаем %20 обратно
+    output="${output//$marker/%20}"
+
+    echo "$output"
 }
 
 if [ $# -ne 1 ]; then
